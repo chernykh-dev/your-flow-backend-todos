3b010afee97b7d4345b24d85d6754bd514e88a88
Chernykh Artyom <chernykhav@bcs.ru>
2025-07-11 01:29:06 +0300

    fix for dockerfile

--- TodoService/Program.cs
+++ TodoService/Program.cs
@@ -3,38 +3,46 @@ using TodoService.Domain.Repositories;
 using TodoService.Infrastructure;
 using TodoService.Infrastructure.Repositories;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace TodoService;
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddMapster();
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
-
-builder.Services.AddDbContext<TodosDbContext>();
-
-builder.Services.AddScoped<ITodoItemRepository, TodoItemRepository>();
-
-builder.Services.AddRouting();
-builder.Services.AddControllers();
-
-builder.Services.AddLogging();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.MapOpenApi();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+        builder.Services.AddOpenApi();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddMapster();
+        builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
+
+        builder.Services.AddDbContext<TodosDbContext>();
+
+        builder.Services.AddScoped<ITodoItemRepository, TodoItemRepository>();
+
+        builder.Services.AddRouting();
+        builder.Services.AddControllers();
+
+        builder.Services.AddLogging();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+        }
+
+        // app.UseHttpsRedirection();
+        app.UseRouting();
+        app.MapControllers();
+
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        app.Run();
+    }
 }
-
-// app.UseHttpsRedirection();
-app.UseRouting();
-app.MapControllers();
-
-app.UseSwagger();
-app.UseSwaggerUI();
-
-app.Run();
